8418469a35058d49e7216d09edc335ede7134376
Chr1stian <christiannyvoll@gmail.com>
2020-01-22 13:13:02 +0100

    Inital application layout using Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
 import React from "react";
-import logo from "./logo.svg";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import "./App.css";
+import { Typography, ListItemIcon } from "@material-ui/core";
+import MailIcon from "@material-ui/icons/Mail";
+
+const drawerWidth = 200;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex"
+    },
+    appBar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.default,
+      padding: theme.spacing(2)
+    }
+  })
+);
 
 const App: React.FC = () => {
+  const classes = useStyles();
+  const [value] = React.useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar className={classes.appBar}>
+        <Tabs value={value} aria-label="task_steps_bar">
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={classes.drawer}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <List>
+          {["Item", "Item", "Item", "Item"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary={text + " " + (index + 1)} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>Lorem ipsum.</Typography>
+        <Typography paragraph>Dolores it.</Typography>
+      </main>
     </div>
   );
 };
