47cfc75ae6315820df9f2a01c66dcc42165c3003
Chr1stian <christiannyvoll@gmail.com>
2020-03-09 16:27:59 +0100

    Added option to change code language

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -3,8 +3,11 @@ import { Controlled as CodeMirror } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import { Task } from "../App";
+import { Select, MenuItem } from "@material-ui/core";
 require("codemirror/mode/xml/xml");
 require("codemirror/mode/javascript/javascript");
+require("codemirror/mode/python/python");
+require("codemirror/mode/clike/clike");
 
 interface CodeInputProps {
   className?: string;
@@ -21,22 +24,35 @@ const CodeInput: React.FC<CodeInputProps> = (props: CodeInputProps) => {
     changeTask(selectedTask + 1, task);
   };
 
+  const [language, setLanguage] = React.useState("javascript");
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>): void => {
+    setLanguage(event.target.value as string);
+  };
+
   return (
-    <CodeMirror
-      className={className}
-      value={task.code}
-      options={{
-        mode: "xml",
-        theme: "material",
-        lineNumbers: true
-      }}
-      onBeforeChange={(_editor, _data, value): void => {
-        setCode(value);
-      }}
-      onChange={(): void => {
-        // console.log("code:", task.code);
-      }}
-    />
+    <div>
+      <Select value={language} onChange={handleChange}>
+        <MenuItem value={"javascript"}>JavaScript</MenuItem>
+        <MenuItem value={"python"}>Python</MenuItem>
+        <MenuItem value={"text/x-java"}>Java</MenuItem>
+        <MenuItem value={"Other"}>Other</MenuItem>
+      </Select>
+      <CodeMirror
+        className={className}
+        value={task.code}
+        options={{
+          mode: language,
+          theme: "material",
+          lineNumbers: true
+        }}
+        onBeforeChange={(_editor, _data, value): void => {
+          setCode(value);
+        }}
+        onChange={(): void => {
+          // console.log("code:", task.code);
+        }}
+      />
+    </div>
   );
 };
 
