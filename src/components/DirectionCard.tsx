e6000c7345b5c2a841e36b07d24d8f471497bb2e
Chr1stian <christiannyvoll@gmail.com>
2020-03-15 21:51:31 +0100

    Restyle DirectionCard

--- src/components/DirectionCard.tsx
+++ src/components/DirectionCard.tsx
@@ -1,19 +1,45 @@
 import React from "react";
-import { Card, Typography, CardContent } from "@material-ui/core";
+import {
+  Card,
+  Typography,
+  CardContent,
+  createStyles,
+  makeStyles
+} from "@material-ui/core";
+import InfoIcon from "@material-ui/icons/Info";
 
 interface DirectionCardProps {
   text: string;
 }
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    card: {
+      display: "flex",
+      flexDirection: "row",
+      padding: "4px"
+    },
+    icon: {
+      color: "secondary"
+    }
+  })
+);
 const DirectionCard: React.SFC<DirectionCardProps> = (
   props: DirectionCardProps
 ) => {
+  const classes = useStyles();
+
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {props.text}
-        </Typography>
-      </CardContent>
+    <Card className={classes.card}>
+      <InfoIcon
+        className={classes.icon}
+        fontSize={"default"}
+        color="secondary"
+        viewBox={"0 0 25 20"}
+      />
+      <Typography style={{ textAlign: "center" }} variant="subtitle1">
+        {props.text}
+      </Typography>
     </Card>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   Drawer,
-  Button,
   List,
   MenuItem,
   ListItemText,
